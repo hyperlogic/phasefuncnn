b3a3fe4c068cf74a94d3037ffff3e8eba8ce92aa
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2024-11-01 10:23:00 -0700

    rotations are closer to being correct, except for glitches

--- preprocess/build_input.py
+++ preprocess/build_input.py
@@ -1,8 +1,27 @@
 import glm
+import math
 import numpy as np
 from tqdm import trange, tqdm
 
 TRAJ_WINDOW = 12
+
+
+def axis_angle_to_quat(axis, angle):
+    n = glm.normalize(axis) * math.sin(angle / 2)
+    w = math.cos(angle / 2)
+    return glm.quat(w, n.x, n.y, n.z)
+
+
+def quat_to_mat4(quat):
+    x_axis = quat * glm.vec3(1, 0, 0)
+    y_axis = quat * glm.vec3(0, 1, 0)
+    z_axis = quat * glm.vec3(0, 0, 1)
+    return glm.mat4(
+        glm.vec4(x_axis, 0),
+        glm.vec4(y_axis, 0),
+        glm.vec4(z_axis, 0),
+        glm.vec4(0, 0, 0, 1),
+    )
 
 
 class Input:
@@ -12,13 +31,20 @@ class Input:
         self.j_pos = np.zeros(shape=(num_joints, 3), dtype=float)
         self.j_vel = np.zeros(shape=(num_joints, 3), dtype=float)
 
+
 class Skeleton:
     def __init__(self, bvh):
         self.joint_names = bvh.get_joints_names()
         self.root_name = self.joint_names[0]
-        self.joint_index_map = {self.joint_names[i]: i for i in range(len(self.joint_names))}
-        self.has_pos_map = {j: "Xposition" in bvh.joint_channels(j) for j in self.joint_names}
-        self.has_rot_map = {j: "Xrotation" in bvh.joint_channels(j) for j in self.joint_names}
+        self.joint_index_map = {
+            self.joint_names[i]: i for i in range(len(self.joint_names))
+        }
+        self.has_pos_map = {
+            j: "Xposition" in bvh.joint_channels(j) for j in self.joint_names
+        }
+        self.has_rot_map = {
+            j: "Xrotation" in bvh.joint_channels(j) for j in self.joint_names
+        }
         self.parent_map = {j: bvh.joint_parent_index(j) for j in self.joint_names}
         self.joint_offset_map = {j: bvh.joint_offset(j) for j in self.joint_names}
         self.num_joints = len(self.joint_names)
@@ -46,7 +72,7 @@ class Skeleton:
 
 
 def build_input(bvh):
-    num_frames = 120 # bvh.nframes
+    num_frames = bvh.nframes
     frame_time = bvh.frame_time
 
     s = Skeleton(bvh)
@@ -61,36 +87,44 @@ def build_input(bvh):
 
         xforms = [glm.mat4() for i in range(s.num_joints)]
 
+        root_trans = glm.vec3(0, 0, 0)
+
         # build xforms
-        root_motion = glm.vec3(0, 0, 0)
         for i in range(s.num_joints):
             joint_name = s.get_joint_name(i)
             offset = s.get_joint_offset(joint_name)
 
             if s.has_pos(joint_name):
-                x_pos = bvh.frame_joint_channel(frame, joint_name, "Xposition") + offset[0]
-                y_pos = bvh.frame_joint_channel(frame, joint_name, "Yposition") + offset[1]
-                z_pos = bvh.frame_joint_channel(frame, joint_name, "Zposition") + offset[2]
-
-                if s.is_root(joint_name):
-                    root_motion += glm.vec3(x_pos, y_pos, z_pos)
-                    x_pos = root_motion[0]
-                    y_pos = root_motion[1]
-                    z_pos = root_motion[2]
+                x_pos = (
+                    bvh.frame_joint_channel(frame, joint_name, "Xposition") + offset[0]
+                )
+                y_pos = (
+                    bvh.frame_joint_channel(frame, joint_name, "Yposition") + offset[1]
+                )
+                z_pos = (
+                    bvh.frame_joint_channel(frame, joint_name, "Zposition") + offset[2]
+                )
             else:
                 x_pos, y_pos, z_pos = offset[0], offset[1], offset[2]
 
-            if (s.has_rot(joint_name)):
-                x_rot = glm.radians(bvh.frame_joint_channel(frame, joint_name, "Xrotation"))
-                y_rot = glm.radians(bvh.frame_joint_channel(frame, joint_name, "Yrotation"))
-                z_rot = glm.radians(bvh.frame_joint_channel(frame, joint_name, "Zrotation"))
+            if s.has_rot(joint_name):
+                x_rot = glm.radians(
+                    bvh.frame_joint_channel(frame, joint_name, "Xrotation")
+                )
+                y_rot = glm.radians(
+                    bvh.frame_joint_channel(frame, joint_name, "Yrotation")
+                )
+                z_rot = glm.radians(
+                    bvh.frame_joint_channel(frame, joint_name, "Zrotation")
+                )
             else:
                 x_rot, y_rot, z_rot = 0, 0, 0
 
-            xrot = glm.mat4(glm.normalize(glm.quat(x_rot, glm.vec3(1, 0, 0))))
-            yrot = glm.mat4(glm.normalize(glm.quat(y_rot, glm.vec3(0, 1, 0))))
-            zrot = glm.mat4(glm.normalize(glm.quat(z_rot, glm.vec3(0, 0, 1))))
-            m = zrot * xrot * yrot
+            m = quat_to_mat4(
+                axis_angle_to_quat(glm.vec3(0, 0, 1), z_rot)
+                * axis_angle_to_quat(glm.vec3(1, 0, 0), x_rot)
+                * axis_angle_to_quat(glm.vec3(0, 1, 0), y_rot)
+            )
             m[3] = glm.vec4(x_pos, y_pos, z_pos, 1)
             parent_index = s.get_parent_index(joint_name)
             if parent_index >= 0:
--- preprocess/visualize_input.py
+++ preprocess/visualize_input.py
@@ -1,11 +1,17 @@
 import glm
 import pygfx as gfx
 from tqdm import trange, tqdm
-
+import sys
+from wgpu.gui.auto import WgpuCanvas, run
 
 cubes = []
 frame = 0
 g_inputs = []
+
+scene = None
+renderer = None
+camera = None
+canvas = None
 
 
 def animate():
@@ -17,9 +23,20 @@ def animate():
     for j in range(len(g_inputs[frame].j_pos)):
         cubes[j].local.position = g_inputs[frame].j_pos[j]
 
+    global renderer, canvas, scene, camera
+    renderer.render(scene, camera)
+    canvas.request_draw()
+
+
+def on_key_down(event):
+    if event.key == "Escape":
+        renderer.target.close()
 
 
 def visualize_input(skeleton, inputs):
+
+    global renderer, canvas, scene, camera
+
     scene = gfx.Scene()
     scene.add(gfx.AmbientLight())
     scene.add(gfx.DirectionalLight())
@@ -36,26 +53,32 @@ def visualize_input(skeleton, inputs):
     joint_colors[skeleton.get_joint_index("LeftToeBase")] = "#0000ff"
     joint_colors[skeleton.get_joint_index("RightToeBase")] = "#ff0000"
 
+    joint_size = [0.5 for j in range(skeleton.num_joints)]
+    joint_size[skeleton.get_joint_index("Head")] = 3
+
     # build cube for every transform
     for j in range(len(inputs[0].j_pos)):
+        box_size = joint_size[j]
         cube = gfx.Mesh(
-            gfx.box_geometry(0.5, 0.5, 0.5),
-            gfx.MeshPhongMaterial(color=joint_colors[j])
+            gfx.box_geometry(box_size, box_size, box_size),
+            gfx.MeshPhongMaterial(color=joint_colors[j]),
         )
         scene.add(cube)
         cube.local.position = inputs[0].j_pos[j]
         cubes.append(cube)
 
-    AXIS_LEN = 10
-    x_axis = gfx.Mesh(gfx.box_geometry(AXIS_LEN, 0.5, 0.5), gfx.MeshPhongMaterial(color="#ff0000"))
-    x_axis.local.position = glm.vec3(AXIS_LEN / 2, 0, 0)
-    y_axis = gfx.Mesh(gfx.box_geometry(0.5, 10.0, 0.5), gfx.MeshPhongMaterial(color="#00ff00"))
-    y_axis.local.position = glm.vec3(0, AXIS_LEN / 2, 0)
-    z_axis = gfx.Mesh(gfx.box_geometry(0.5, 0.5, 10.0), gfx.MeshPhongMaterial(color="#0000ff"))
-    z_axis.local.position = glm.vec3(0, 0, AXIS_LEN / 2)
+    scene.add(gfx.helpers.AxesHelper(10.0, 0.5))
+    scene.add(gfx.helpers.GridHelper(size=100))
 
-    scene.add(x_axis)
-    scene.add(y_axis)
-    scene.add(z_axis)
+    canvas = WgpuCanvas()
+    renderer = gfx.renderers.WgpuRenderer(canvas)
+    controller = gfx.OrbitController(camera, register_events=renderer)
 
-    gfx.show(scene, before_render=animate)
+    camera.local.position = (10, 1, 0)
+    camera.look_at((0, 0, 0))
+
+
+    renderer.add_event_handler(on_key_down, "key_down")
+
+    canvas.request_draw(animate)
+    run()
