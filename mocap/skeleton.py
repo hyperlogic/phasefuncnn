75325761112e42cef4fae1f9d56eeda93cc4a1a2
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2024-12-06 08:09:07 -0800

    remove comment

--- mocap/skeleton.py
+++ mocap/skeleton.py
@@ -60,34 +60,3 @@ class Skeleton:
                 self.mirror_map.append(mirror_index)
             else:
                 self.mirror_map.append(i)
-
-
-"""
-    // build mirror map.
-    _nonMirroredIndices.clear();
-    _mirrorMap.reserve(_jointsSize);
-    for (int i = 0; i < _jointsSize; i++) {
-        if (_joints[i].name != "Hips" && _joints[i].name != "Spine" &&
-            _joints[i].name != "Spine1" && _joints[i].name != "Spine2" &&
-            _joints[i].name != "Neck" && _joints[i].name != "Head" &&
-            !((_joints[i].name.startsWith("Left") || _joints[i].name.startsWith("Right")) &&
-              _joints[i].name != "LeftEye" && _joints[i].name != "RightEye")) {
-            // HACK: we don't want to mirror some joints so we remember their indices
-            // so we can restore them after a future mirror operation
-            _nonMirroredIndices.push_back(i);
-        }
-        int mirrorJointIndex = -1;
-        if (_joints[i].name.startsWith("Left")) {
-            QString mirrorJointName = QString(_joints[i].name).replace(0, 4, "Right");
-            mirrorJointIndex = nameToJointIndex(mirrorJointName);
-        } else if (_joints[i].name.startsWith("Right")) {
-            QString mirrorJointName = QString(_joints[i].name).replace(0, 5, "Left");
-            mirrorJointIndex = nameToJointIndex(mirrorJointName);
-        }
-        if (mirrorJointIndex >= 0) {
-            _mirrorMap.push_back(mirrorJointIndex);
-        } else {
-            _mirrorMap.push_back(i);
-        }
-    }
-"""
