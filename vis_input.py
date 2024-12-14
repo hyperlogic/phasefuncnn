de01d00f0491634d6162231b871af010cb17dc72
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2024-12-14 11:31:44 -0800

    use TypedDict for input_view

--- vis_input.py
+++ vis_input.py
@@ -1,8 +1,7 @@
-from dataclasses import dataclass, field, asdict
 import os
 import pickle
 import sys
-from typing import Tuple
+from typing import Tuple, TypedDict
 
 import numpy as np
 import pygfx as gfx
@@ -15,19 +14,15 @@ OUTPUT_DIR = "output"
 TRAJ_WINDOW_SIZE = 12
 TRAJ_ELEMENT_SIZE = 4
 
+class ColumnView(TypedDict):
+    size: int
+    indices: list[int]
 
-@dataclass
-class ColumnView:
-    size: int = 0
-    indices: list[int] = field(default_factory=list)
-
-
-@dataclass
-class InputView:
-    traj_pos_i: ColumnView = field(default_factory=ColumnView)
-    traj_vel_i: ColumnView = field(default_factory=ColumnView)
-    joint_pos_im1: ColumnView = field(default_factory=ColumnView)
-    joint_vel_im1: ColumnView = field(default_factory=ColumnView)
+class InputView(TypedDict):
+    traj_pos_i: ColumnView
+    traj_vel_i: ColumnView
+    joint_pos_im1: ColumnView
+    joint_vel_im1: ColumnView
 
 
 class RenderBuddy:
@@ -45,23 +40,23 @@ def build_column_indices(start: int, stride: int, repeat: int = 1) -> Tuple[int,
 def build_input_view(skeleton: mocap.Skeleton) -> InputView:
     num_joints = skeleton.num_joints
 
-    input_view = InputView()
+    input_view = {}
     offset = 0
     next_offset, indices = build_column_indices(offset, 4, TRAJ_WINDOW_SIZE)
-    input_view.traj_pos_i = ColumnView(2, indices)
+    input_view["traj_pos_i"] = {"size": 2, "indices": indices}
     _, indices = build_column_indices(offset + 2, 4, TRAJ_WINDOW_SIZE)
-    input_view.traj_vel_i = ColumnView(2, indices)
+    input_view["traj_vel_i"] = {"size": 2, "indices": indices}
 
     offset = next_offset
     next_offset, indices = build_column_indices(offset, 6, num_joints)
-    input_view.joint_pos_im1 = ColumnView(3, indices)
+    input_view["joint_pos_im1"] = {"size": 3, "indices": indices}
     _, indices = build_column_indices(offset + 3, 6, num_joints)
-    input_view.joint_vel_im1 = ColumnView(3, indices)
+    input_view["joint_vel_im1"] = {"size": 3, "indices": indices}
 
-    for k, v in asdict(input_view).items():
+    for k, v in input_view.items():
         print(f"{k}: {v}")
 
-    return input_view
+    return InputView(**input_view)
 
 
 if __name__ == "__main__":
@@ -80,5 +75,5 @@ if __name__ == "__main__":
     X = torch.load(os.path.join(OUTPUT_DIR, "X.pth"), weights_only=True)
     print(f"X.shape = {X.shape}")
 
-    num_cols = input_view.joint_vel_im1.indices[-1] + input_view.joint_vel_im1.size
+    num_cols = input_view["joint_vel_im1"]["indices"][-1] + input_view["joint_vel_im1"]["size"]
     assert num_cols == X.shape[1]
