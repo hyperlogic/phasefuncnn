50a3ff057838879aea74f6add1ff7479cdac89df
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2024-12-21 17:52:04 -0800

    added vis_output to draw Y tensor

--- vis_output.py
+++ vis_output.py
@@ -2,7 +2,6 @@ import os
 import pickle
 import sys
 from typing import Tuple, TypedDict
-from time import perf_counter
 
 import numpy as np
 import pygfx as gfx
@@ -29,35 +28,40 @@ class ColumnView(TypedDict):
     indices: list[int]
 
 
-class InputView(TypedDict):
-    traj_pos_i: ColumnView
-    traj_vel_i: ColumnView
-    joint_pos_im1: ColumnView
-    joint_vel_im1: ColumnView
+class OutputView(TypedDict):
+    traj_pos_ip1: ColumnView
+    traj_dir_ip1: ColumnView
+    joint_pos_i: ColumnView
+    joint_vel_i: ColumnView
+    root_vel_i: ColumnView
+    root_angvel_i: ColumnView
+    joint_angvel_i: ColumnView
+    phase_vel_i: ColumnView
+    contacts_i: ColumnView
 
 
-def ref(row: torch.Tensor, input_view: InputView, key: str, index: str) -> torch.Tensor:
-    index = input_view[key]["indices"][index]
-    size = input_view[key]["size"]
+def ref(row: torch.Tensor, output_view: OutputView, key: str, index: str) -> torch.Tensor:
+    index = output_view[key]["indices"][index]
+    size = output_view[key]["size"]
     return row[index : index + size]
 
 
-class VisInputRenderBuddy(RenderBuddy):
+class VisOutputRenderBuddy(RenderBuddy):
     skeleton: Skeleton
-    input_view: InputView
-    X: torch.Tensor
+    output_view: OutputView
+    Y: torch.Tensor
     row_group: gfx.Group
     row: int
     camera: gfx.PerspectiveCamera
     canvas: WgpuCanvas
     playing: bool
 
-    def __init__(self, skeleton: Skeleton, input_view: InputView, X: torch.Tensor):
+    def __init__(self, skeleton: Skeleton, output_view: OutputView, Y: torch.Tensor):
         super().__init__()
 
         self.skeleton = skeleton
-        self.input_view = input_view
-        self.X = X
+        self.output_view = output_view
+        self.Y = Y
 
         self.row_group = gfx.Group()
         self.scene.add(self.row_group)
@@ -74,14 +78,15 @@ class VisInputRenderBuddy(RenderBuddy):
 
         positions = []
         colors = []
-        X_row = self.X[row]
+
+        Y_row = self.Y[row]
         for child in skeleton.joint_names:
             child_index = skeleton.get_joint_index(child)
             parent_index = skeleton.get_parent_index(child)
             if parent_index >= 0:
                 # line from p.offset
-                p0 = ref(X_row, self.input_view, "joint_pos_im1", parent_index).tolist()
-                p1 = ref(X_row, self.input_view, "joint_pos_im1", child_index).tolist()
+                p0 = ref(Y_row, self.output_view, "joint_pos_i", parent_index).tolist()
+                p1 = ref(Y_row, self.output_view, "joint_pos_i", child_index).tolist()
                 positions += [p0, p1]
                 colors += [[1, 1, 1, 1], [0.5, 0.5, 1, 1]]
         joint_line = gfx.Line(
@@ -91,8 +96,8 @@ class VisInputRenderBuddy(RenderBuddy):
         positions = []
         colors = []
         for i in range(TRAJ_WINDOW_SIZE - 1):
-            p0 = ref(X_row, self.input_view, "traj_pos_i", i)
-            p1 = ref(X_row, self.input_view, "traj_pos_i", i + 1)
+            p0 = ref(Y_row, self.output_view, "traj_pos_ip1", i)
+            p1 = ref(Y_row, self.output_view, "traj_pos_ip1", i + 1)
             positions += [[p0[0], 0.0, p0[1]], [p1[0], 0.0, p1[1]]]
             if i % 2 == 0:
                 colors += [[1, 1, 1, 1], [1, 1, 1, 1]]
@@ -111,7 +116,7 @@ class VisInputRenderBuddy(RenderBuddy):
         super().on_animate(dt)
         if self.playing:
             row = self.row + 1
-            if row >= self.X.shape[0]:
+            if row >= self.Y.shape[0]:
                 row = 0
             self.retain_row(row)
 
@@ -128,32 +133,49 @@ class VisInputRenderBuddy(RenderBuddy):
         super().on_dpad_right()
         print("DPAD RIGHT!")
 
+
 def build_column_indices(start: int, stride: int, repeat: int = 1) -> Tuple[int, list[int]]:
     indices = [i * stride + start for i in range(repeat)]
     offset = repeat * stride + start
     return offset, indices
 
 
-def build_input_view(skeleton: Skeleton) -> InputView:
+def build_output_view(skeleton: Skeleton) -> OutputView:
     num_joints = skeleton.num_joints
 
-    input_view = {}
+    output_view = {}
     offset = 0
     next_offset, indices = build_column_indices(offset, 4, TRAJ_WINDOW_SIZE)
-    input_view["traj_pos_i"] = {"size": 2, "indices": indices}
+    output_view["traj_pos_ip1"] = {"size": 2, "indices": indices}
     _, indices = build_column_indices(offset + 2, 4, TRAJ_WINDOW_SIZE)
-    input_view["traj_vel_i"] = {"size": 2, "indices": indices}
+    output_view["traj_dir_ip1"] = {"size": 2, "indices": indices}
 
     offset = next_offset
-    next_offset, indices = build_column_indices(offset, 6, num_joints)
-    input_view["joint_pos_im1"] = {"size": 3, "indices": indices}
-    _, indices = build_column_indices(offset + 3, 6, num_joints)
-    input_view["joint_vel_im1"] = {"size": 3, "indices": indices}
+    next_offset, indices = build_column_indices(offset, 9, num_joints)
+    output_view["joint_pos_i"] = {"size": 3, "indices": indices}
+    _, indices = build_column_indices(offset + 3, 9, num_joints)
+    output_view["joint_vel_i"] = {"size": 3, "indices": indices}
+    _, indices = build_column_indices(offset + 6, 9, num_joints)
+    output_view["joint_angvel_i"] = {"size": 3, "indices": indices}
 
-    for k, v in input_view.items():
+    offset = next_offset
+    next_offset, indices = build_column_indices(offset, 3, 1)
+    output_view["root_vel_i"] = {"size": 2, "indices": indices}
+    _, indices = build_column_indices(offset + 2, 3, 1)
+    output_view["root_angvel_i"] = {"size": 1, "indices": indices}
+
+    offset = next_offset
+    next_offset, indices = build_column_indices(offset, 1, 1)
+    output_view["phase_vel_i"] = {"size": 1, "indices": indices}
+
+    offset = next_offset
+    next_offset, indices = build_column_indices(offset, 4, 1)
+    output_view["contacts_i"] = {"size": 4, "indices": indices}
+
+    for k, v in output_view.items():
         print(f"{k}: {v}")
 
-    return InputView(**input_view)
+    return OutputView(**output_view)
 
 
 if __name__ == "__main__":
@@ -166,20 +188,19 @@ if __name__ == "__main__":
 
     # unpickle/load data
     skeleton = unpickle_obj(outbasepath + "_skeleton.pkl")
-    input_view = build_input_view(skeleton)
+    output_view = build_output_view(skeleton)
 
     print(f"skeleton.num_joints = {skeleton.num_joints}")
-    X = torch.load(os.path.join(OUTPUT_DIR, "X.pth"), weights_only=True)
-    X_mean = torch.load(os.path.join(OUTPUT_DIR, "X_mean.pth"), weights_only=True)
-    X_std = torch.load(os.path.join(OUTPUT_DIR, "X_std.pth"), weights_only=True)
-    X_w = torch.load(os.path.join(OUTPUT_DIR, "X_w.pth"), weights_only=True)
-    print(f"X.shape = {X.shape}")
+    Y = torch.load(os.path.join(OUTPUT_DIR, "Y.pth"), weights_only=True)
+    Y_mean = torch.load(os.path.join(OUTPUT_DIR, "Y_mean.pth"), weights_only=True)
+    Y_std = torch.load(os.path.join(OUTPUT_DIR, "Y_std.pth"), weights_only=True)
+    print(f"Y.shape = {Y.shape}")
 
-    num_cols = input_view["joint_vel_im1"]["indices"][-1] + input_view["joint_vel_im1"]["size"]
-    assert num_cols == X.shape[1]
+    num_cols = output_view["contacts_i"]["indices"][-1] + output_view["contacts_i"]["size"]
+    assert num_cols == Y.shape[1]
 
     # un-normalize input for visualiztion
-    X = X * (X_std / X_w) + X_mean
+    Y = Y * Y_std + Y_mean
 
-    render_buddy = VisInputRenderBuddy(skeleton, input_view, X)
+    render_buddy = VisOutputRenderBuddy(skeleton, output_view, Y)
     run()
